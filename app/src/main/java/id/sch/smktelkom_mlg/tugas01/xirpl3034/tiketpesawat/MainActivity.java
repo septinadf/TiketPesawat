ec3d7a1587865c20cfaf7fa8d7b4d54e784f9f04
User <septinarika90@gmail.com>
2016-09-12 22:57:59 +0700

    first commit

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3034/tiketpesawat/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3034/tiketpesawat/MainActivity.java
@@ -2,13 +2,22 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3034.tiketpesawat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
+
+
+
+
+
+
